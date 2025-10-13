853454a4ab9fdbdb2183e591eb5c4edc328d1026
Nikolay K <sirbulgat@gmail.com>
2025-10-13 23:10:24 +0300

    fetch useEffect

--- src/pages/homePages/homePages.tsx
+++ src/pages/homePages/homePages.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { useEffect } from "react";
 
 interface Props {}
 
+interface IUser {
+    UserId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
 const HomePage = (props: Props) => {
+
+    const [data, setData] = useState([] as IUser[]);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData();
+    },);
+
+
     return (
-    <div>Home</div>
+        <>
+        <div>Home</div>
+            <div> {data.map((item) => ( <div key={item.id}> <h2>- {item.title}</h2> <p>{item.body}</p> </div> ))} </div>
+        </>
     )
 }
 export default HomePage;
